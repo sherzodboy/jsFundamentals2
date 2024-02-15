b17ed6eb4b494f7195cbdf2c9f4e46d51b9eb5f6
sherzodboy <kholmamatovsh09@gmail.com>
2024-02-15 10:19:24 +0500

    Strict Mode and Functions

---
+++ script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("Yes I can drive");
+
+// const interface = "Auido";
+// const private = 124;
+// const if = "12"
+
+// function logger() {
+//   console.log("My name is Tom");
+// }
+// // calling, running, invoking
+// logger();
+// logger();
+// logger();
+
+// function logger(firstName) {
+//   console.log(`My name is ${firstName}`);
+// }
+
+// logger("Tom");
+// logger("John");
+// logger("Bob");
+
+// function logger(firstName, age) {
+//   console.log(`My name is ${firstName} and ${age}yoshdaman`);
+// }
+
+// logger("Tom", 15, "uzbek");
+
+// function calc(a, b) {
+//   return a * b;
+// }
+
+// const multipleCalc = calc(5, 6);
+// console.log(multipleCalc);
+// console.log(calc(5, 6));
+
+// function declaration
+// function calcAge1(birthYear) {
+//   const age1 = 2024 - birthYear;
+//   return age1;
+// }
+// console.log(calcAge1(2000));
+// //function expression
+// const calcAge2 = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+// console.log(calcAge2(1990));
+
+// arrow function =>
+// const calcAge3 = (birthYear) => {
+//   return 2024 - birthYear;
+// };
+// console.log(calcAge3(1999));
+
+// all functions
+
+// function age1() {}
+// const age2 = function () {};
+// const age3 = () => {};
+
+// const retirementAge = (birthYear) => {
+//   const retirement = 65;
+//   const age = 2024 - birthYear;
+//   return retirement - age;
+// };
+
+// const findRetirAge = retirementAge(2009);
+// console.log(findRetirAge);
+
+// const cutFruit = function (fruit) {
+//   return fruit * 4;
+// };
+
+// const fruitProcessor = (apples, oranges) => {
+//   const cutApples = cutFruit(apples);
+//   const cutOranges = cutFruit(oranges);
+//   const juice = `Juice with ${cutApples} piece of apple and ${cutOranges} piece oranges`;
+//   return juice;
+// };
+
+// console.log(fruitProcessor(4, 3));
