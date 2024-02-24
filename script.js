770487630d7936e4abe345e531f5bb55bd850917
sherzodboy <kholmamatovsh09@gmail.com>
2024-02-24 09:04:02 +0500

    hometask answered

--- script.js
+++ script.js
@@ -618,3 +618,34 @@
 
 // let num = Math.trunc(Math.random() * 4);
 // console.log(num);
+
+// choychqava va umumiy hisobni hisoblash orta arifmetigini
+const calcTip = function (bill) {
+  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  return tip;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    // sum = sum + arr[i]
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage(bills));
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
